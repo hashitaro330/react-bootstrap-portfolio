7ce5169c4f04dbeed326bacb43e5fb3f5870691c
hashitaro330 <hsmt.nttd.330@gmail.com>
2023-01-04 20:59:09 +0900

    first commit

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <Button>ボタン</Button>
+    </div>
+  );
+};
+
+export default HomePage;
